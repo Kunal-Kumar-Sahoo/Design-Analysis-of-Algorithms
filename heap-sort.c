7a16ee4becc41e2bd2779694a83b6d7279e0b47b
Kunal Kumar Sahoo <kunal.sahoo2003@gmail.com>
2023-04-06 17:39:24 +0530

    heap sort

--- heap-sort.c
+++ heap-sort.c
@@ -6,40 +6,53 @@ void swap(int *a, int *b) {
     *b = temp;
 }
 
-void heapify(int arr[], int n, int i) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-
-    if(left < n && arr[left] > arr[largest])
-        largest = left;
-
-    if(right < n && arr[right] > arr[largest])
-        largest = right;
+void max_heapify(int arr[], int size, int i) {
+    int l = 2 * i + 1, r = 2 * i + 2, largest = i;
+    
+    if(l < size && arr[l] > arr[i])
+        largest = l;
+    
+    if(r < size && arr[r] > arr[largest])
+        largest = r;
 
     if(largest != i) {
-        swap(arr + i, arr + largest);
-        heapify(arr, n, largest);
+        swap(arr+largest, arr+i);
+        max_heapify(arr, size, largest);
     }
 }
 
-void heap_sort(int arr[], int n) {
-    for(int i = n / 2 - 1; i >= 0; --i)
-        heapify(arr, n, i);
+void build_max_heap(int arr[], int size) {
+    for(int i = size / 2; i >= 0; --i)
+        max_heapify(arr, size, i);
+}
 
-    for(int i = n - 1; i >= 0; --i) {
+int extract_max(int arr[], int size) {
+    if(size >= 1) {
+        int max = arr[0];
+        arr[0] = arr[size-1];
+        size--;
+        max_heapify(arr, size, 0);
+
+        return max;
+    }
+}
+
+void heap_sort(int arr[], int size) {
+    build_max_heap(arr, size);
+    for(int i = size-1; i > 0; --i) {
         swap(arr, arr+i);
-        heapify(arr, i, 0);
+        size--;
+        max_heapify(arr, size, 0);
     }
 }
 
 int main() {
-    printf("Enter size of the array: ");
+    printf("Enter the size of the array: ");
     int n; scanf("%d", &n);
 
     int arr[n];
-    printf("Enter %d elements in the array:\n", n);
 
+    printf("Enter %d elements in the array:\n", n);
     for(int i = 0; i < n; ++i)
         scanf("%d", arr+i);
 
